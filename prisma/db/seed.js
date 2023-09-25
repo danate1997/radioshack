ec14e5a038e8be0af6d5d37abbb7ede037eed73d
danate1997 <daniel.atehortua1997@gmail.com>
2023-09-25 13:42:08 -0400

    seeded

---
+++ prisma/db/seed.js
@@ -0,0 +1,34 @@
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+
+async function main() {
+    // for (let i = 1; i <= 2; i++) {
+    //   const user = await prisma.user.create({
+    //     data: {
+    //       email: `email${i}`,
+    //       password: `password${i}`,
+    //     },
+    //   });
+      for (let j = 1; j <= 4; j++) {
+      const product = await prisma.product.create({
+        data: {
+          name: `name${j}`,
+          price: 100,
+          details: `details${j}`
+        },
+      });
+    }
+}
+// }
+
+main().catch(e => {
+    console.log(e);
+    process.exit(1)
+}) .finally(async() => {
+    await prisma.$disconnect()
+})
+
+
+
---
+++ prisma/migrations/20230925165304_radioshack/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(65,30),
+    "details" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "isCart" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "id" SERIAL NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
